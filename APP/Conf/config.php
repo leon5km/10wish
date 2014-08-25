ff0904eaa37b43cfe7a0ad1a8824d2c1470c22ec
leon5km <leon5km@163.com>
2014-08-25 14:13:45 +0800

    开启独立分组2
    调整文件位置。

--- APP/Conf/config.php
+++ APP/Conf/config.php
@@ -1,6 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	/*
+	 * 普通分组配置，区别与独立分组
+	 */
 	'APP_GROUP_LIST' => 'Home,Admin,Test,Njb,PHP_API',
 	'DEFAULT_GROUP' => 'Home',
 	
@@ -9,8 +12,8 @@ return array(
 	*/
 	'APP_GROUP_MODE' => 1,
 	'APP_GROUP_PATH' => 'Modules',
-    'APP_GROUP_LIST'=>'Index',      //项目分组列表
-    'DEFAULT_GROUP'=>'Index',         //默认分组
+    'APP_GROUP_LIST'=>'Home,Admin,Test,Njb,PHP_API',      //项目分组列表
+    'DEFAULT_GROUP'=>'Home',         //默认分组
 		
 	//定义错误页面
 	//'TMPL_EXCEPTION_FILE'=>'./App/Tpl/Public/error.html' // 定义公共错误模板
